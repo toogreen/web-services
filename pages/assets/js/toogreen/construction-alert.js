5147d55c31587d391570024db9ddc8226bfb4f2f
David Gagnon <toogreen@gmail.com>
2020-06-20 15:50:37 -0400

    changed the construction alert text slightly to emphasize that this is still under heavy construction and a lot will change

--- pages/assets/js/toogreen/construction-alert.js
+++ pages/assets/js/toogreen/construction-alert.js
@@ -1,5 +1,5 @@
 // Create lang variable from the script above
-var construction = "This website is under construction. The content you see now is subject to a lot of changes and/or irrelevant at the moment. Please come back later.";
+var construction = "WARNING: This website is still under heavy construction. The content you see now is subject to a lot of changes and/or almost completely irrelevant at the moment. Please come back later.";
 
 // Display the value in a popup
 alert(construction);

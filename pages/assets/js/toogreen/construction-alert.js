f96a2f74097bde5900ac5778ea8ef5d4f4c9fade
David Gagnon <toogreen@gmail.com>
2020-06-20 15:41:52 -0400

    added a Javascript alert to let people know the website is still under construction for now

---
+++ pages/assets/js/toogreen/construction-alert.js
@@ -0,0 +1,5 @@
+// Create lang variable from the script above
+var construction = "This website is under construction. The content you see now is subject to a lot of changes and/or irrelevant at the moment. Please come back later.";
+
+// Display the value in a popup
+alert(construction);
